ac1f40a79b0f11022f0630f5fb197365b28baf7b
larry-xue <azoux0311@gmail.com>
2024-05-21 23:15:06 +0800

    fix: landing page

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <div class="flex justify-center items-center h-full">
         <div class="text-white">
           <p class="text-3xl font-bold mb-4 text-center text-green-500">Welcome to my ai bot site</p>
-          <p class="text-3xl font-bold text-gray-300">Please select a page to start</p>
+          <p class="text-3xl font-bold text-gray-300 text-center">Please select a page to start</p>
           <div class="flex mt-4 gap-2 justify-center">
             <UButton to="/chat" color="green">Chat</UButton>
             <UButton color="violet" variant="solid" to="/translate">Translate</UButton>

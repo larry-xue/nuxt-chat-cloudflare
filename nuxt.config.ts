217ba7ac58be97be429b082ea70a0bc5cb9dcddd
larry-xue <azoux0311@gmail.com>
2024-06-24 23:59:56 +0800

    fix: remove auth config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', "@sidebase/nuxt-auth", '@pinia/nuxt',],
-  auth: {
-    baseURL: process.env.AUTH_ORIGIN
-  }
+
 })

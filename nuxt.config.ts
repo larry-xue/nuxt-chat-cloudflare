c5fbaa78f15a3c5f949f7e8697be6c1320bb1b7b
larry-xue <azoux0311@gmail.com>
2024-06-25 00:09:19 +0800

    fix: auth provider

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', "@sidebase/nuxt-auth", '@pinia/nuxt',],
-
+  auth: {
+    provider: {
+      type: 'authjs',
+      defaultProvider: 'github',
+    },
+    baseURL: process.env.AUTH_ORIGIN,
+  }
 })

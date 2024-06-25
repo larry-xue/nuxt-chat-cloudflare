f54c211ad4f97d74c384aed54f1a2fdde9fe05be
larry-xue <azoux0311@gmail.com>
2024-06-25 12:31:42 +0800

    feat: remove auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', "@sidebase/nuxt-auth", '@pinia/nuxt',],
-  auth: {
-    provider: {
-      type: 'authjs',
-      defaultProvider: 'github',
-    },
-    baseURL: process.env.AUTH_ORIGIN,
-  }
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+  ],
 })
